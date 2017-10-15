844ac82ea366d57293098edc798074872d6dd02d
Cristian Fabbri <cristian.fabbri5@studio.unibo.it>
2017-10-16 01:54:19 +0200

    Created CameraFollow to make MainCamera follow the Player

---
+++ Project (Un)Matter/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	private Transform playerTransform;
+
+	void Start(){
+		playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+	//after Update()
+	void LateUpdate(){
+		transform.position = playerTransform.position;
+	}
+}
